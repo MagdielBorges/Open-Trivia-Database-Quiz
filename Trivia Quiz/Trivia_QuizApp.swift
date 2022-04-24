b89e8cdfb7feba99cc8ff5afd2a099a48f2e2cd1
Magdiel Borges <magdiel.borges@icloud.com>
2022-04-24 09:55:54 -0400

    Initial Commit

---
+++ Trivia Quiz/Trivia_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_QuizApp.swift
+//  Trivia Quiz
+//
+//  Created by Magdiel Borges on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
